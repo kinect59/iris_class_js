b4a651dc4458cf9665d04346a83049758c4148aa
kinect59 <hazem.wannous@univ-lille1.fr>
2020-09-01 16:17:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const test_data = tf.tensor2d([[6.7, 2.5, 5.8, 1.8]]);
   model = await tf.loadLayersModel('web_model/model.json').then(model => {
   
   model.summary();
-  y = model.predict(tf.tensor2d([[6.7, 2.5, 5.8, 1.8]]))
-  y_pred = y.argMax()
+  //y = model.predict(tf.tensor2d([[6.7, 2.5, 5.8, 1.8]]))
+  //y_pred = y.argMax()
   //document.getElementById('out').innerHTML = y_pred.dataSync()
   document.getElementById('micro-out-div').innerText = model.predict(tf.tensor2d([[6.7, 2.5, 5.8, 1.8]])).argMax().dataSync();
   //y = model.predict(tf.zeros([1,2])) 
